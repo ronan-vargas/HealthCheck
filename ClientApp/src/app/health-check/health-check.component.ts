d4441f71527daf3d95a7289337569da37bc8cbd6
Ronan Vargas <ronan.vargas@gmail.com>
2022-09-27 21:08:46 -0300

    137/747 - Create components for the Angular CLI.

--- ClientApp/src/app/health-check/health-check.component.ts
+++ ClientApp/src/app/health-check/health-check.component.ts
@@ -1,1 +1,33 @@
-﻿
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-health-check',
+  templateUrl: './health-check.component.html',
+  styleUrls: ['./health-check.component.css']
+})
+export class HealthCheckComponent implements OnInit {
+  public result: Result;
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string) {
+  }
+  ngOnInit() {
+    this.http.get<Result>(this.baseUrl + 'hc').subscribe(result => {
+      this.result = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Check {
+  name: string;
+  status: string;
+  responseTime: number;
+}
+
+interface Result {
+  checks: Check[];
+  totalStatus: string;
+  totalResponseTime: number;
+}
+
